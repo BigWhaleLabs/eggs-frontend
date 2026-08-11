b634c4923beb4e989fc66d79468030739d69665d
Nikita Kolmogorov <backmeupplz@gmail.com>
2026-08-11 07:27:02 -0700

    Remove Chicken minting from the mini app (#14)

--- src/helpers/shutdownAuth.ts
+++
@@ -1,9 +0,0 @@
-import { getAddress } from 'viem'
-
-export function getShutdownAuthorizationMessage(address: `0x${string}`) {
-  return [
-    'Eggs shutdown authorization',
-    '',
-    `Wallet: ${getAddress(address)}`,
-  ].join('\n')
-}
